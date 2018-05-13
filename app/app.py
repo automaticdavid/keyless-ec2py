8155f62480678da328b758bc47158ead74343309
zen <zen@localhost.localdomain>
2018-05-13 12:03:58 -0400

    flask wrapper for ec2.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restplus import Resource, Api, fields, reqparse
 import subprocess
+import json
 
 __author__ = "David CLAUVEL"
 __version__ = "1.0"
@@ -17,9 +18,5 @@ ns = api.namespace('api', description='Keyless API access to dynamic inventories
 @ns.route('/ec2.py', methods=['GET'])
 class ListCollects(Resource):
     def get(self):
-        # res =Ec2Inventory()
-        # res = dyn_inventory_ec2.Ec2Inventory()
-        # print("res", res) 
-        # return {"yes": "ok"}
         res=subprocess.check_output("./ec2.py", stderr=subprocess.STDOUT)
-        return(res)
+        return(json.loads(res))
